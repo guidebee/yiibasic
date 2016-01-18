8fb527c8fad8c5316bfd1366c34d1ddac293319a
James Shen <james.shen@guidebee.com>
2016-01-18 18:15:34 +0800

    Working with Databases

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=10.1.1.12;dbname=yii2basic',
+    'username' => 'dbuser',
+    'password' => '!@#Password',
     'charset' => 'utf8',
 ];
